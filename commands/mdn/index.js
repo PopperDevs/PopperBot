946fe493c28b8275d1fd0a554c7efaaf7af7b11c
Jody LeCompte <jody@jodylecompte.com>
2020-07-07 16:51:48 -0500

    Add MDN command from prototype

---
+++ commands/mdn/index.js
@@ -0,0 +1,48 @@
+const { parse } = require('node-html-parser');
+const fetch = require('node-fetch');
+
+const { permissionType, commandType } = require('../../lib/permissions');
+
+module.exports = {
+  name: 'mdn',
+  aliases: ['mdn'],
+  type: commandType.base.name,
+  permissions: permissionType.user,
+  template: 'mdn',
+  async handler({ Discord, client, message }) {
+    const args = message.content.slice(4);
+    const encodedQuery = encodeURIComponent(args);
+
+    const url = `https://developer.mozilla.org/en-US/search?q=${encodedQuery}`;
+
+    const res = await fetch(`${url}`);
+    const text = await res.text();
+    const data = parse(text);
+
+    const resultTitles = data.querySelectorAll('.result-title');
+    const resultExcerpts = data.querySelectorAll('.result-excerpt');
+    // const resultUrls = data.querySelectorAll('.result a');
+
+    if (resultTitles.length === 0) {
+      return message.channel.send('No results found.');
+    }
+
+    const resultCount = Math.min(5, resultTitles.length);
+
+    const embed = new Discord.MessageEmbed()
+      .setAuthor(`Search results for ${args}`, client.user.avatarURL())
+      .setColor('#6F39B0')
+      .setDescription('I found the following results');
+
+    for (let i = 0; i < resultCount; i += 1) {
+      const title = resultTitles[i].innerHTML;
+      const excerpt = resultExcerpts[i].innerHTML.replace(/<[^>]*>?/gm, '');
+      const url =
+        'https://developer.mozilla.org/' + resultTitles[i].getAttribute('href');
+
+      embed.addField(title, `${excerpt}\n${url}`);
+    }
+
+    message.channel.send(embed);
+  },
+};
