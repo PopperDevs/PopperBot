dfa59c9ee7c5d5213a65c9e2701582b6e87f23f2
Caleb Miller <caleb@mathmammoth.com>
2020-07-08 12:11:21 -0600

    :pencil2: Added a colon after "I found the following results" in mdn command

--- commands/mdn/index.js
+++ commands/mdn/index.js
@@ -32,7 +32,7 @@ module.exports = {
     const embed = new Discord.MessageEmbed()
       .setAuthor(`Search results for ${args}`, client.user.avatarURL())
       .setColor('#6F39B0')
-      .setDescription('I found the following results')
+      .setDescription('I found the following results:')
       .setTimestamp(message.createdAt)
       .setFooter(`mdn | ${message.author.tag}`);
 
