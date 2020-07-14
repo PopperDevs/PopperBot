842e56612f1ae2608659bc638fa9eda2b2a4624a
Caleb Miller <caleb@mathmammoth.com>
2020-07-13 19:01:32 -0600

    Added info commands

---
+++ commands/roleinfo/index.js
@@ -0,0 +1,36 @@
+const { permissionType, commandType } = require('../../lib/permissions');
+const { getRole } = require('../../lib/utils');
+
+module.exports = {
+  name: 'roleinfo',
+  aliases: [],
+  type: commandType.base.name,
+  permissions: permissionType.user,
+  template: 'roleinfo [role]',
+  async handler({ Discord, message, args }) {
+    const role = await getRole(message, args.join(' '));
+    if (!role) {
+      message.channel.send(
+        new Discord.MessageEmbed()
+          .setColor('#FF9AA2')
+          .setFooter(`roleinfo | ${message.author.tag}`)
+          .setTimestamp(message.createdAt)
+          .setTitle('No role found.')
+      );
+    } else {
+      message.channel.send(
+        new Discord.MessageEmbed()
+          .setColor('#6F39B0')
+          .setFooter(`roleinfo | ${message.author.tag}`)
+          .setTimestamp(message.createdAt)
+          .addField('Name', `<@&${role.id}>`, true)
+          .addField('ID', role.id, true)
+          .addField('Hoisted', role.hoist ? 'Yes' : 'No', true)
+          .addField('Color', role.hexColor, true)
+          .addField('Mentionable', role.mentionable ? 'Yes' : 'No', true)
+          .addField('Users in Role: ', role.members.size, true)
+          .addField('Created at', role.createdAt.toLocaleString(), true)
+      );
+    }
+  },
+};
---
+++ commands/userinfo/index.js
@@ -0,0 +1,40 @@
+const { permissionType, commandType } = require('../../lib/permissions');
+const { getUser } = require('../../lib/utils');
+
+module.exports = {
+  name: 'userinfo',
+  aliases: ['whois'],
+  type: commandType.base.name,
+  permissions: permissionType.user,
+  template: 'userinfo [user]',
+  async handler({ Discord, message, args }) {
+    const u = message.guild.member(
+      (await getUser(message, args.join(' '))) || message.author
+    );
+    message.channel.send(
+      new Discord.MessageEmbed()
+        .setAuthor(u.user.tag, u.user.displayAvatarURL())
+        .setColor('#6F39B0')
+        .setFooter(`userinfo | ${message.author.tag}`)
+        .setTimestamp(message.createdAt)
+        .setThumbnail(u.user.displayAvatarURL())
+        .addField('ID', u.user.id)
+        .addField(
+          'Permissions',
+          u.permissions
+            .toArray()
+            .map((x) =>
+              x
+                .toLowerCase()
+                .split('_')
+                .map((y) => y.replace(/^./, (z) => z.toUpperCase()))
+                .join(' ')
+            )
+            .join(', ')
+        )
+        .addField('Roles', u._roles.map((x) => `<@&${x}>`).join(', '))
+        .addField('Joined At', u.joinedAt.toLocaleString(), true)
+        .addField('Registered At', u.user.createdAt.toLocaleString(), true)
+    );
+  },
+};
